2d3d3df938b3a6eb7343e5904bb40a217000ae50
System Administrator <root@nyabugandas-MacBook-Pro.local>
2020-10-01 12:11:02 -0600

    created

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <jokeButton />
+    <div id="choices">
+      <button @click="selection(1)"> loud </button>
+      <button @click="selection(2)"> snake </button>
+      <button @click="selection(3)" > normal </button>
+    </div>
+    <loudJoke v-if=" choice == 1" />
+    <normalJoke v-if=" choice == 3" />
+    <snakeJoke v-if=" choice == 2" />
+
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import jokeButton from "./components/jokebutton";
+import snakeJoke from "./components/snakejoke";
+import normalJoke from "./components/normaljoke";
+import loudJoke from "./components/loudjoke";
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    loudJoke,
+    normalJoke,
+    snakeJoke,
+    jokeButton
+  },
+  data : function(){
+    return {
+      choice : 3
+    }
+  },
+  methods: {
+    selection(choice) {
+      this.choice = choice
+      
+    }
+  },
 };
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/loudjoke.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h4> {{ joke }}</h4>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "loud-joke",
+        computed: {
+            joke() {
+                return this.$store.getters.loud; 
+            }
+        },
+    }
+</script>
+
+<style scoped>
+</style>
